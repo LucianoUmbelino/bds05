0850b3b935b5d4059dd4cf89308e32a2c93172d9
Luciano Umbelino <luciano.umbelino@gmail.com>
2021-11-17 15:27:27 -0300

    Error resolved - Challenge concluded

--- src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
@@ -44,9 +44,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, VISITOR_OR_MEMBER).permitAll()
 		.antMatchers(VISITOR_OR_MEMBER).hasAnyRole("VISITOR", "MEMBER")
-		.antMatchers(MEMBER).hasAnyRole("MEMBER")
+		.antMatchers(HttpMethod.GET, VISITOR_OR_MEMBER).permitAll()
+			
 		.anyRequest().authenticated();
 	}
 
